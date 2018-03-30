c7e326c47feebe40413b3b8cd439ae2af2538a7a
NCUTCodingMan <1667254420@qq.com>
2018-03-30 13:48:34 +0800

    添加thymeleaf视图解析器、模板引擎以及模板解析器

--- src/main/java/spitter/config/WebConfig.java
+++ src/main/java/spitter/config/WebConfig.java
@@ -8,6 +8,11 @@ import org.springframework.web.servlet.config.annotation.DefaultServletHandlerCo
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
 
 /**
  * 配置类
@@ -21,8 +26,13 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 public class WebConfig extends WebMvcConfigurerAdapter {
     /**
      * spitter.config view resolver
+     * InternalResourceViewResolver 一般用于解析JSP,通常会设置前缀与后缀作为资源路径信息
+     * 在带有"/"，可作为路径看待
+     * InternalResourceViewResolver实现了ViewResolver接口，视图解析器将控制器中的字符串
+     * 转化为了具体的视图信息，链接了逻辑视图与物理视图
      * @return view resolver
      */
+    /*
     @Bean
     public ViewResolver viewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -30,6 +40,44 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         viewResolver.setSuffix(".jsp");
         viewResolver.setExposeContextBeansAsAttributes(true);
         return viewResolver;
+    }
+    */
+
+    /**
+     * 配置Thymeleaf视图解析器
+     * @param templateEngine
+     * @return
+     */
+    @Bean
+    public ViewResolver viewResolver(SpringTemplateEngine templateEngine) {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine);
+        return viewResolver;
+    }
+
+    /**
+     * 模板引擎
+     * @param templateResolver
+     * @return
+     */
+    @Bean
+    public SpringTemplateEngine templateEngine(SpringResourceTemplateResolver templateResolver) {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+        return templateEngine;
+    }
+
+    /**
+     * 模板解析器
+     * @return
+     */
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setPrefix("/WEB-INF/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        return templateResolver;
     }
 
     @Override
